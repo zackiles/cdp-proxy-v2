4dc353db9ed1822b831fc57f160a05807a54bdfb
Zachary Iles <zack.iles@gmail.com>
2025-02-19 19:34:13 -0500

    fx

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 Deno.env.set('DEBUG', 'pw:protocol')
+Deno.env.set('BROWSER_MANAGER_DEBUG', '1')
+Deno.env.set('DENO_ENV', 'test')
+
+import { chromium } from '@browser-tools/browser-manager'
+
+const url = await chromium.getDownloadUrl({
+  platform: 'windows',
+  arch: 'x64',
+})
+console.log(url)
+await chromium.install({
+  platform: 'windows',
+  arch: 'x64',
+  customBasePath: '.cache',
+})
