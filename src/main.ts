106d92394a0c3834a408039ad4e701a60efbb723
Zachary Iles <zack.iles@gmail.com>
2025-02-22 20:31:49 -0500

    fx

--- src/main.ts
+++ src/main.ts
@@ -16,19 +16,17 @@ console.log('installed')
 import { getAvailablePort } from '@std/net'
 const ac = new AbortController()
 //import { ProxyAgent } from './proxy-agent.ts'
-import { ProxyRewriter } from './proxy-rewriter.ts'
+import { replaceInResponse } from '@zackiles/response-rewriter'
 import { BrowserManager } from './browser-manager.ts'
 
 const config = {
   proxyPort: Number(Deno.env.get('CDP_PROXY_PORT')) || getAvailablePort(),
   browserPort: Number(Deno.env.get('BROWSER_PORT')) || getAvailablePort(),
   hostname: Deno.env.get('CDP_PROXY_HOSTNAME') || '0.0.0.0',
-  browserExecutablePath: Deno.env.get('BROWSER_EXECUTABLE_PATH')
+  browserExecutablePath: Deno.env.get('BROWSER_EXECUTABLE_PATH'),
 }
-const proxyRewriter = new ProxyRewriter(config.proxyPort, config.hostname)
 
-
-const httpHandler = (req: Request): Promise<Response | null> => {
+const httpHandler = (req: Request): Promise<Response> => {
   console.log(Deno.inspect(req))
   return Promise.resolve(new Response('Hello, world'))
 }
@@ -36,44 +34,49 @@ const httpHandler = (req: Request): Promise<Response | null> => {
 const server = Deno.serve({
   port: config.proxyPort,
   hostname: config.hostname,
-  handler: proxyRewriter.handler([httpHandler]),
+  handler: httpHandler,
   signal: ac.signal,
   onListen({ port, hostname }) {
     console.log(`Server started at http://${hostname}:${port!}`)
   },
 })
 
-const browserManager = new BrowserManager(config.hostname, config.browserPort, config.browserExecutablePath)
+const browserManager = new BrowserManager(
+  config.hostname,
+  config.browserPort,
+  config.browserExecutablePath,
+)
 const { browser, browserWebSocketDebuggerUrl } = await browserManager.start()
 console.log(`Browser debugger url started at ${browserWebSocketDebuggerUrl}`)
 
 // Add signal handlers for graceful shutdown
 const handleShutdown = async () => {
-  console.log(`Closing server at http://${config.hostname}:${config.proxyPort}...`)
+  console.log(
+    `Closing server at http://${config.hostname}:${config.proxyPort}...`,
+  )
   ac.abort()
   await server.shutdown()
-};
+}
 
 // Add signal handlers for graceful shutdown
-Deno.addSignalListener("SIGINT", handleShutdown);
-Deno.addSignalListener("SIGTERM", handleShutdown);
+Deno.addSignalListener('SIGINT', handleShutdown)
+Deno.addSignalListener('SIGTERM', handleShutdown)
 
 // Handle uncaught exceptions
-addEventListener("unhandledrejection", (event: PromiseRejectionEvent) => {
-  console.error("Unhandled rejection:", event.reason);
-  handleShutdown();
-});
-
-addEventListener("error", (event: ErrorEvent) => {
-  console.error("Uncaught exception:", event.error);
-  handleShutdown();
-});
-
-server.finished.then(() => {
-  console.log("Server closed!")
-  Deno.exit(0)
+addEventListener('unhandledrejection', (event: PromiseRejectionEvent) => {
+  console.error('Unhandled rejection:', event.reason)
+  handleShutdown()
 })
 
+addEventListener('error', (event: ErrorEvent) => {
+  console.error('Uncaught exception:', event.error)
+  handleShutdown()
+})
+
+server.finished.then(() => {
+  console.log('Server closed!')
+  Deno.exit(0)
+})
 
 /**
 const proxy = new ProxyAgent()
